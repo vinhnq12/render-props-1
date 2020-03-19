1d5a612f8d58f63b4bb3609cc538e0537c43b168
vinhnq12 <31241502+vinhnq12@users.noreply.github.com>
2020-03-19 10:11:12 +0700

    rrr
    rrr

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Lists data={data} render={item => <div>{item}</div>} />
+    <Lists data={data} render={item => <div>- {item}</div>} />
   </React.StrictMode>,
   rootElement
 );
