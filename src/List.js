100c1331d72cc4c5a8c6587ae7719ec46be2ec52
vinhnq12 <31241502+vinhnq12@users.noreply.github.com>
2020-03-19 10:23:04 +0700

    Update
    update

---
+++ src/List.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function({ item }) {
+  return <div>{item}</div>;
+}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import Lists from "./Lists";
+import List from "./List";
+import List1 from "./List1";
 
 const data = ["a", "b", "c"];
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Lists data={data} render={item => <div>{item}</div>} />
-    <Lists data={data} render={item => <div>- {item}</div>} />
+    <Lists data={data} render={item => <List item={item} />} />
+    <Lists data={data} render={item => <List1 item={item} />} />
   </React.StrictMode>,
   rootElement
 );
